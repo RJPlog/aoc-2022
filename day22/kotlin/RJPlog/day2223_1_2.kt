c42e1b249f0486ec5f8d057dbfecbc06049e3caf
RJPlog <tfrenz67@gmail.com>
2025-01-14 22:39:08 +0100

    Update day2223_1_2.kt
    part 2 solved

--- day22/kotlin/RJPlog/day2223_1_2.kt
+++ day22/kotlin/RJPlog/day2223_1_2.kt
@@ -51,101 +51,91 @@ fun proposeMove(elves: List<String>, rules: List<Char>, pos: String): Char {
     return '0'
 }
 
-fun diffusion(): Int {
+fun diffusion(part: Int = 0): Int {
 
-var elvePos = mutableListOf<String>()  // hätte ich gerne mit Pair / listOf<Int> gemacht, aber bekomme ich nicht in binary search gebacken
+    var elvePos = mutableListOf<String>()  // hätte ich gerne mit Pair / listOf<Int> gemacht, aber bekomme ich nicht in binary search gebacken
 
-var ruleSet = mutableListOf('N', 'S', 'W', 'E')
+    var ruleSet = mutableListOf('N', 'S', 'W', 'E')
 
-var x = 0
-var y = 0
-var xMin = 0
-var xMax = 0
-var yMin = 0
-var yMax = 0
+    var x = 0
+    var y = 0
+    var xMin = 0
+    var xMax = 0
+    var yMin = 0
+    var yMax = 0
 
-File("day2223_puzzle_input.txt").forEachLine {
-    it.forEach {
-        if (it == '#') {
-            elvePos.add(x.toString()+","+y.toString())
+    File("day2223_puzzle_input.txt").forEachLine {
+        it.forEach {
+            if (it == '#') {
+                elvePos.add(x.toString()+","+y.toString())
+            }
+            x += 1
+            xMax = x
         }
-        x += 1
-        xMax = x
+        x = 0
+        y += 1
+        yMax = y
     }
-    x = 0
-    y += 1
-    yMax = y
-}
 
-elvePos.sort()
-// println(elvePos)
-//     for (yy in yMin..yMax) {
-//     for (xx in xMin..xMax) {
-//         if (elvePos.binarySearch((xx).toString()+","+(yy).toString()) >= 0) print("#") else print(".")
-//     }
-//     println()
-// }
-// println()
+    elvePos.sort()
 
+    var i = 0
+    while (true) {
+        var elvePosNew = mutableListOf<String>()
+        elvePos.forEach{
+            var x = it.split(",")[0].toInt()
+            var y = it.split(",")[1].toInt()
 
-for (i in 0..9) {
+            when (proposeMove(elvePos, ruleSet, it)) {
+                'N' -> if (proposeMove(elvePos, ruleSet, x.toString() + "," + (y-2).toString()) == 'S') elvePosNew.add(it) else elvePosNew.add(x.toString()+","+(y-1).toString())
+                'S' -> if (proposeMove(elvePos, ruleSet, x.toString() + "," + (y+2).toString()) == 'N') elvePosNew.add(it) else elvePosNew.add(x.toString()+","+(y+1).toString())
+                'W' -> if (proposeMove(elvePos, ruleSet, (x-2).toString() + "," + (y).toString()) == 'E') elvePosNew.add(it) else elvePosNew.add((x-1).toString()+","+(y).toString())
+                'E' -> if (proposeMove(elvePos, ruleSet, (x+2).toString() + "," + (y).toString()) == 'W') elvePosNew.add(it) else elvePosNew.add((x+1).toString()+","+(y).toString())
+                '0' -> elvePosNew.add(it)
+            }
+        }
+        
+        elvePosNew.sort()
 
-var elvePosNew = mutableListOf<String>()
-elvePos.forEach{
-    var x = it.split(",")[0].toInt()
-    var y = it.split(",")[1].toInt()
-//     println("   $it -> ${proposeMove(elvePos, ruleSet, it)},   $ruleSet")
-    when (proposeMove(elvePos, ruleSet, it)) {
-        'N' -> if (proposeMove(elvePos, ruleSet, x.toString() + "," + (y-2).toString()) == 'S') elvePosNew.add(it) else elvePosNew.add(x.toString()+","+(y-1).toString())
-        'S' -> if (proposeMove(elvePos, ruleSet, x.toString() + "," + (y+2).toString()) == 'N') elvePosNew.add(it) else elvePosNew.add(x.toString()+","+(y+1).toString())
-        'W' -> if (proposeMove(elvePos, ruleSet, (x-2).toString() + "," + (y).toString()) == 'E') elvePosNew.add(it) else elvePosNew.add((x-1).toString()+","+(y).toString())
-        'E' -> if (proposeMove(elvePos, ruleSet, (x+2).toString() + "," + (y).toString()) == 'W') elvePosNew.add(it) else elvePosNew.add((x+1).toString()+","+(y).toString())
-        '0' -> elvePosNew.add(it)
+        i += 1
+        if (part == 2) {
+            if (elvePosNew == elvePos) {
+                println("no more moves")
+                return i
+            } 
+        } else if (part == 1) {
+            if (i > 9 ) break
+        }
+
+        elvePos.clear()
+        elvePos.addAll(elvePosNew)
+        var swap = ruleSet[0]
+        ruleSet.add(swap)
+        ruleSet.removeAt(0)
     }
-}
-elvePosNew.sort()
-//println(elvePos)
-if (elvePosNew == elvePos) {
-    println("no more moves")
-    break
-}
-//println(elvePosNew)
-elvePos.clear()
-elvePos.addAll(elvePosNew)
-var swap = ruleSet[0]
-ruleSet.add(swap)
-ruleSet.removeAt(0)
 
-}
-
-elvePos.forEach{
-    if (it.split(",")[0].toInt() < xMin) xMin = it.split(",")[0].toInt()
-    else if (it.split(",")[0].toInt() > xMax) xMax = it.split(",")[0].toInt()
-    if (it.split(",")[1].toInt() < yMin) yMin = it.split(",")[1].toInt()
-    else if (it.split(",")[1].toInt() > yMax) yMax = it.split(",")[1].toInt()
-}
-
-for (yy in yMin..yMax) {
-    for (xx in xMin..xMax) {
-        if (elvePos.binarySearch((xx).toString()+","+(yy).toString()) >= 0) print("#") else print(".")
+    elvePos.forEach{
+        if (it.split(",")[0].toInt() < xMin) xMin = it.split(",")[0].toInt()
+        else if (it.split(",")[0].toInt() > xMax) xMax = it.split(",")[0].toInt()
+        if (it.split(",")[1].toInt() < yMin) yMin = it.split(",")[1].toInt()
+        else if (it.split(",")[1].toInt() > yMax) yMax = it.split(",")[1].toInt()
     }
-    println()
-}
-println()
 
-println("$xMax, $xMin, $yMax, $yMin, ${elvePos.size}")
-return (xMax-xMin+1) * (yMax-yMin+1) - elvePos.size
+    return (xMax-xMin+1) * (yMax-yMin+1) - elvePos.size
 }
 
 fun main() {
 
- var t1 = System.currentTimeMillis()
+    var t1 = System.currentTimeMillis()
 
-println("--- Day 23: Unstable Diffusion ---")
+    println("--- Day 23: Unstable Diffusion ---")
 
-var solution1 = diffusion()
-println("   the rectangle contains $solution1 empty tiles")  // 241 is to low  //4246 is to high
-   
-t1 = System.currentTimeMillis() - t1
-println("puzzle solved in ${t1} ms")
+    var solution1 = diffusion(1)
+    println("   the rectangle contains $solution1 empty tiles") 
+    
+    var solution2 = diffusion(2)
+    println("   it takes $solution2 steps until no elve moves")
+
+    t1 = System.currentTimeMillis() - t1
+    println("puzzle solved in ${t1} ms")
 }
