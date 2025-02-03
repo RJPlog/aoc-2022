03cae671b4134d47682e78c4f82781c20aa631f0
RJPlog <tfrenz67@gmail.com>
2025-02-03 12:12:20 +0100

    Update day2216_1_2.kt

--- day16/kotlin/RJPlog/day2216_1_2.kt
+++ day16/kotlin/RJPlog/day2216_1_2.kt
@@ -2,9 +2,44 @@
 // kotlinc day2216_1_2.kt -include-runtime -d day2216_1_2.jar && java -jar -Xss515m day2216_1_2.jar
 
 import java.io.File
+import kotlin.math.*
 
 var roads = mutableMapOf<String, Int>()
+var reducedRoads = mutableMapOf<String,Int>()
 var pumps = mutableMapOf<String, Int>()
+
+// reduce map of roads to all connetections between pumps/pumps and startpoint/pumps
+fun reduceRoads(key1: String, intKey: String, key2: String, roadsLocal: MutableMap<String, Int>, count: Int) {
+
+    if (roads.containsKey(intKey+"-"+key2) || roads.containsKey(key2+"-"+intKey)) {
+        if (reducedRoads.contains(key1+"-"+key2)) {
+            reducedRoads.put(key1+"-"+key2, min(count, reducedRoads.getValue(key1+"-"+key2)))
+        } else {
+            reducedRoads.put(key1+"-"+key2, count)
+        }
+        if (reducedRoads.contains(key2+"-"+key1)) {
+            reducedRoads.put(key2+"-"+key1, min(count, reducedRoads.getValue(key2+"-"+key1)))
+        } else {
+            reducedRoads.put(key2+"-"+key1, count)
+        }
+    } else {
+        // deal with pumps more than one 
+        for ((key,value) in roadsLocal) {
+            var rLNew = mutableMapOf<String, Int>()
+            if (key.take(2) == intKey) {
+                var key1New = key.takeLast(2)
+                rLNew.putAll(roadsLocal)
+                rLNew.remove(key)
+                reduceRoads(key1, key1New, key2, rLNew, count + 1)
+            } else if (key.takeLast(2) == intKey) {
+                var key1New = key.take(2)
+                rLNew.putAll(roadsLocal)
+                rLNew.remove(key)
+                reduceRoads(key1, key1New, key2, rLNew, count + 1)
+            }
+        }
+    }
+}
 
 fun move(sC: String, tL: Int, pumps: MutableMap<String, Int>): Int { 
     //println("${" ".repeat(30-tL)} $sC")
@@ -43,6 +78,7 @@ fun aocDay2216(part: Int = 1): Int {
             junctions.add(listOf(junctOne, junctTwo).sorted().joinToString("-"))
         }
         val flowRate = it.substringAfter("rate=").substringBefore(";").toInt()
+        //if (flowRate > 0) pumps.put(junctOne, flowRate)
         pumps.put(junctOne, flowRate)
     }
     junctions = junctions.distinct().sorted().toMutableList()
@@ -50,17 +86,41 @@ fun aocDay2216(part: Int = 1): Int {
     junctions.forEach{
         allRoads.put(it, 1)
     }
+    println("allRoads")
     println(allRoads)
 
-    // #1.2 remove "unreal junctions" with no "flow rate"
+    // #1.2 make roadmap global
+        roads.putAll(allRoads)
 
-    // #1.3 make roadmap global
-    roads.putAll(allRoads)
+    // #1.3 determine shortest ways from entry to pumps with positive flow rate and from each pumpt to pumpt 
+    for ((key1,value1) in pumps) {
+        if (key1 == "AA" || value1 > 0) {
+            for ((key2,value2) in pumps) {
+                if (key2 == "AA" || value2 > 0) {
+                    if ((key1 != key2)) {
+                        reduceRoads(key1, key1, key2, roads, 1)
+                    }
+                }
+            }
+        }
+    }
+    println("reducedRoads")
+    println(reducedRoads)
+
+
+    // or complete diffent approach, determine way from all pumps to all others. 
+    // run loops number of pump times and calculate possible pressure, skip all combinations which have no direct connect.
+
+
+
+    println()
+    println("roads / pumps")
     println(roads)
     println(pumps)
 
+
     val startCave = "AA"
-    val timeLimit = 9
+    val timeLimit = 5
 
     // #2 evaluate 
     var result = 0
@@ -72,7 +132,7 @@ fun aocDay2216(part: Int = 1): Int {
     } 
 
     return result
-}
+}   
 
 fun main() {
 
